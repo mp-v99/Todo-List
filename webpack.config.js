99b6048edb980c7893facbf68c5153fcb05a1fce
mpv <193361506+mp-v99@users.noreply.github.com>
2026-02-02 20:32:10 -0600

    Fix .js file path

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
-  entry: "~/src/main.js",
+  entry: "./src/index.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
@@ -12,7 +12,7 @@ module.exports = {
   },
   plugins: [
 	  new HtmlWebpackPlugin({
-		  template: "./src/template.html"
+		  template: "./src/index.html"
 	  }),
 	],
 }
