6174d87f378f15af51c25bc54f51b2345104825a
mpv <193361506+mp-v99@users.noreply.github.com>
2026-02-04 18:46:24 -0600

    Feat: Define todo object

---
+++ src/index.js
@@ -0,0 +1,11 @@
+class TodoList {
+    constructor(title, description, dueDate, status) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.status = status;
+    }
+
+
+}
+
