08dea099e521cd33fc1a01f28add6098b583d7f9
mpv <193361506+mp-v99@users.noreply.github.com>
2026-02-04 21:23:02 -0600

    Feat: add list class

--- src/index.js
+++ src/index.js
@@ -1,14 +1,30 @@
+class TodoList {
+    constructor(title, dueDate, status) {
+        this.title = title;
+        this.dueDate = dueDate;
+        this.status = status;
+        this.listItems = [];    
+    }
+
+    addListItem() {
+        const listItem = prompt("Add one list item if you'd like(you can re-prompt to add more items if you'd like)");
+        this.listItems.push(listItem);
+
+        alert(this.listItems)
+    }
+
+}
+
+
 class Project {
     constructor(title, description, dueDate, status, todoLists, notes) {
         this.title = title;
         this.description = description;
         this.dueDate = dueDate;
         this.status = status;
-        this.todoLists = todoLists;
-        this.notes = notes;
+        this.todoLists = [];
+        this.notes = [];
     }
-
-
 }
 
 const drawProject = function(todo) {
@@ -20,7 +36,7 @@ const drawProject = function(todo) {
 `)
 }
 
-const groceries = new Project("groceries", "this is a groceries project", "May-5th-2026", "To do", [{listItem: "Todo List"}], [{note: "Note"}]) 
+const groceries = new Project("groceries", "this is a groceries project", "May-5th-2026", "To do") 
 drawProject(groceries)
 
 window.groceries = groceries;
