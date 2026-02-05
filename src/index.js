33d9697563439516fedf0d91276950f344490883
mpv <193361506+mp-v99@users.noreply.github.com>
2026-02-04 20:17:27 -0600

    Feat: Add logger for todo

--- src/index.js
+++ src/index.js
@@ -6,6 +6,21 @@ class TodoList {
         this.status = status;
     }
 
+    todoInfo() {
+        drawTodo(this)
+    }
+
 
 }
 
+const drawTodo = function(todo) {
+    console.log(`
+|Title:     ${todo.title}               
+|Body:      ${todo.description}              
+|Due:       ${todo.dueDate}                 
+|Status:    ${todo.status}             
+`)
+}
+
+const groceriesList = new TodoList("groceries", "this is a groceries list", "May-5th-2026", "To do") 
+groceriesList.todoInfo();
