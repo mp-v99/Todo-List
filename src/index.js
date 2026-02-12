c8389a3143023653858b6da8289ecd53321c4794
mpv <193361506+mp-v99@users.noreply.github.com>
2026-02-12 14:33:12 -0600

    Feat: Add drawTodo function and test

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ class Project {
         this.notes = [];
     }
 
-    addTodo(title, description, dueDate, listStatus) {     
+    addTodo(title, description, dueDate, listStatus, priority) {     
 
-        const todo = new Todo(title, description, dueDate, listStatus);
+        const todo = new Todo(title, description, dueDate, listStatus, priority);
         this.todos.push(todo)
        
     }
@@ -63,6 +63,22 @@ const drawProject = function(project) {
     for (const note of project.notes) {console.log(`${note.textBody}`)};
 }
 
+const drawTodo = function(todo) {
+    console.log(`
+        |Title:     ${todo.title}               
+        |Description:      ${todo.description}              
+        |Due:       ${todo.dueDate}         
+        |Status: ${todo.status}
+        |Priority: ${todo.priority}
+        `)
+
+    console.log(`    
+        |Checklist:           
+        `)
+    
+        for (const check of todo.checkList) {console.log(`[]${check}`)};
+}
+
 
 // Logs:
 
@@ -74,4 +90,8 @@ groceries.addTodo("buy protein", "the protein has to come in frozen format in or
 groceries.addTodo("buy candy", "the BBB store has good offers for valentine's", "Feb-6th", "To do", "high");
 groceries.addNote("Remember to check the pantry before leaving to check if you are missing something.");
 
+groceries.todos[0].addListItem("carrot");
+groceries.todos[0].addListItem("brocoli");
+
 drawProject(groceries)
+drawTodo(groceries.todos[0])
