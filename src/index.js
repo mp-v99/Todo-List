b1dced74e8e1910d968b774a530a02170abb9de6
mpv <193361506+mp-v99@users.noreply.github.com>
2026-02-12 13:59:31 -0600

    Feat: add Note class and method

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
+class Project {
+    constructor(title, description, createdAt) {
+        this.title = title;
+        this.description = description;
+        this.createdAt = createdAt;
+        this.todos = [];
+        this.notes = [];
+    }
+
+    addTodo(title, description, dueDate, listStatus) {     
+
+        const todo = new Todo(title, description, dueDate, listStatus);
+        this.todos.push(todo)
+       
+    }
+
+    addNote(text) {
+
+        const note = new Note(text);
+        this.notes.push(note);
+    }
+}
+
 class Todo {
     constructor(title, description, dueDate, status, priority) {
         this.title = title;
@@ -14,21 +37,9 @@ class Todo {
 
 }
 
-
-class Project {
-    constructor(title, description, createdAt) {
-        this.title = title;
-        this.description = description;
-        this.createdAt = createdAt;
-        this.todos = [];
-        this.notes = [];
-    }
-
-    addTodo(title, description, dueDate, listStatus) {     
-
-        const todo = new Todo(title, description, dueDate, listStatus);
-        this.todos.push(todo)
-       
+class Note {
+    constructor(textBody) {
+        this.textBody = textBody;
     }
 }
 
@@ -36,16 +47,31 @@ const drawProject = function(project) {
     console.log(`
     |Title:     ${project.title}               
     |Body:      ${project.description}              
-    |Created at:       ${project.createdAt}       
+    |Created at:       ${project.createdAt}         
+    `)
+    
+    console.log(`    
     |List of Todos:           
     `)
 
-    for (const todo of project.todos) {console.log(`${todo.title}`)}
+    for (const todo of project.todos) {console.log(`${todo.title}`)};
+
+    console.log(`
+    |List of Notes:
+    `)
+
+    for (const note of project.notes) {console.log(`${note.textBody}`)};
 }
 
-const groceries = new Project("groceries", "this is a groceries project", "today") 
+
+// Logs:
+
+const groceries = new Project("groceries", "this is a groceries project", "today");
 
 window.groceries = groceries;
-groceries.addTodo("buy veggies", "Feb-6th", "To do", "high")
+groceries.addTodo("buy veggies", "go to the street market to get fresh veggies", "Feb-6th", "To do", "high");
+groceries.addTodo("buy protein", "the protein has to come in frozen format in order to last", "Feb-6th", "To do", "high");
+groceries.addTodo("buy candy", "the BBB store has good offers for valentine's", "Feb-6th", "To do", "high");
+groceries.addNote("Remember to check the pantry before leaving to check if you are missing something.");
 
 drawProject(groceries)
